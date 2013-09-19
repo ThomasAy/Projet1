e3db00f55ba0f57cf36ee2307b162edb3a98d7d4
ThomasAy <thomas@dubware.net>
2013-09-19 16:48:20 +0200

    First commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+function title($titre)
+{
+
+echo '<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
+        \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head lang=\"FR\" xml:lang=\"FR\">
+	<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
+	<title>'.$titre.'</title>
+	<link rel="stylesheet" href="styles.css" />
+
+</head>
+';
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+	include 'functions.php';
+?>
+<?php
+	title("Accueil");
+?>
+<body>
+
+	
+	<link href='http://fonts.googleapis.com/css?family=Mr+Dafoe' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Amaranth:700' rel='stylesheet' type='text/css'>
+	
+	<link rel="stylesheet" href="styles.css" />
+	
+	<span id="sl_play" class="sl_command">&nbsp;</span>
+	<span id="sl_pause" class="sl_command">&nbsp;</span>
+	<span id="sl_i1" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i2" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i3" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i4" class="sl_command sl_i">&nbsp;</span>
+	
+	
+	<section id="slideshow">
+		<div class="container">
+			<div class="c_slider"></div>
+			<div class="slider">
+				<figure>
+					<img src="img/dummy-640x310-1.jpg" alt="" width="640" height="310" />
+					<figcaption>The mirror of soul</figcaption>
+				</figure><!--
+				--><figure>
+					<img src="img/dummy-640x310-2.jpg" alt="" width="640" height="310" />
+					<figcaption>Let's cross that bridge when we come to it</figcaption>
+				</figure><!--
+				--><figure>
+					<img src="img/dummy-640x310-3.jpg" alt="" width="640" height="310" />
+					<figcaption>Sushi<em>(do)</em> time</figcaption>
+				</figure><!--
+				--><figure>
+					<img src="img/dummy-640x310-4.jpg" alt="" width="640" height="310" />
+					<figcaption>Waking Life</figcaption>
+				</figure>
+			</div>
+		</div>
+		
+		<span id="timeline"></span>
+		
+		<ul class="dots_commands"><!--
+			--><li><a title="Show slide 1" href="#sl_i1">Slide 1</a></li><!--
+			--><li><a title="Show slide 2" href="#sl_i2">Slide 2</a></li><!--
+			--><li><a title="Show slide 3" href="#sl_i3">Slide 3</a></li><!--
+			--><li><a title="Show slide 4" href="#sl_i4">Slide 4</a></li>
+		</ul>
+		
+	</section>
+	
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+	<script type="text/javascript" src="jquery.sharrre-1.3.4.min.js"></script>
+</body>
+</html>
