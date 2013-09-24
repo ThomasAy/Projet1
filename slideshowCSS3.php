d3b2b2d4eeddbae56316d937c6a47c54c17722ec
ThomasAy <thomas@dubware.net>
2013-09-24 16:38:20 +0200

    A little try of another slider

---
+++ slideshowCSS3.php
@@ -0,0 +1,37 @@
+<?php
+	include 'functions.php';
+?>
+<?php
+	title("Accueil");
+?>
+
+<body>
+
+<div style="position: relative;" class="slideshow grid_12">
+	<div class="holder" style="">
+	
+	<!-- Start WOWSlider.com BODY section -->
+	<div id="wowslider-container">
+	<div class="ws_images"><ul>
+<li><a href="#overview"><img src="images/data/images/slide1new.jpg" alt="Image Slider" title="" /></a></li>
+<li><a href="demo.html"><img src="images/data/images/slide2new.jpg" alt="jQuery Slider" title="Beautiful Skins" /></a>Killer Effects</li>
+<li><a href="demo.html"><img src="images/data/images/slide3new.jpg" alt="HTML5 Slider" title="" /></a></li>
+<li><a href="http://wowslider.com/wowslider-free-setup.zip"><img src="images/data/images/slide4new.jpg" alt="NO Coding" title="GUI Wizard" /></a>For Windows & Mac</li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#"><img src="images/data/tooltips/slide1new.jpg" alt="CSS3 Slider"/></a>
+<a href="#"><img src="images/data/tooltips/slide2new.jpg" alt="CSS Slideshow"/></a>
+<a href="#"><img src="images/data/tooltips/slide3new.jpg" alt="HTML Image Slider"/></a>
+<a href="#"><img src="images/data/tooltips/slide4new.jpg" alt="CSS Gallery"/></a>
+</div></div>
+<a class="wsl" href="http://wowslider.com">Image Slider</a>
+	</div>
+	<script type="text/javascript" src="images/engine/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
+
+	</div>
+</div>
+
+
+</body>
+</html>
