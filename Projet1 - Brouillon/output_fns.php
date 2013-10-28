e6449b08e73c55ad7a5661e2bb3b73b56c627b23
robbie1186 <roberto.ramos@hotmail.ch>
2013-10-28 17:04:31 +0100

    removing stuff

--- Projet1 - Brouillon/output_fns.php
+++ Projet1 - Brouillon/output_fns.php
@@ -573,8 +573,7 @@ function display_admin_menu(){
 <?php
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 function do_man_category(){
 ?>
   <div class="LogoTop">
@@ -645,11 +644,6 @@ function do_man_category(){
 }
 		
 			
-       
-=======
->>>>>>> 98418da08871c46d05964dcb1625f5713e041ed1
-=======
->>>>>>> 98418da08871c46d05964dcb1625f5713e041ed1
 function display_signup_form($languages_vars){
   //display
 ?>
