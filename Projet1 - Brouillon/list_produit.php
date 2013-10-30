afc5040e5cd613be3517bd6732e35a7c17bc78f1
ThomasAy <thomas@dubware.net>
2013-10-30 18:44:05 +0100

    removing isset session

--- Projet1 - Brouillon/list_produit.php
+++ Projet1 - Brouillon/list_produit.php
@@ -1,9 +1,7 @@
 <?php
  require_once('ghl_fns.php');
  session_start();
- if(isset($_SESSION['mail'])){
- 	header('Location: index.php');
- } else{
+
  do_html_header($languages_var, "Liste des produits");
 
  display_list_of_products();
